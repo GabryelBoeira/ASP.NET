1f7e4854134d66c648e793aa567456a055bd9e58
1720221 <1720221@DCIC116964>
2018-05-10 21:24:36 -0300

    primeiro commit

---
+++ MeuSiteAsp/Model/Atividade.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace Model
+{
+    public class Atividade
+    {
+        public int IdAtividade { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public DateTime Datalimite { get; set; }
+        public Categoria _Categoria { get; set; }
+
+
+    }
+}
