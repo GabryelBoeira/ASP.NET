84f554c1853fa9317c3e6a8b5f2b79ea89b8db54
Gabryel <gabryel392@gmail.com>
2018-05-17 22:01:45 -0300

    Corrigido erro ao Create

--- Aulas/Aula1505_MVC1/Controllers/CategoriasController.cs
+++ Aulas/Aula1505_MVC1/Controllers/CategoriasController.cs
@@ -1,4 +1,5 @@
 ﻿using Aula1505_MVC1.Models;
+using Aula1505_MVC1;
 using System.Collections.Generic;
 using System.Web.Mvc;
 
@@ -6,13 +7,13 @@ namespace Aula1505_MVC1.Controllers
 {
     public class CategoriasController : Controller
     {
-        // GET: Categorias
+        [HttpGet]
         public ActionResult Index()
         {
             List<Categoria> categorias = new List<Categoria>();
 
             categorias.Add(
-                
+
                 new Categoria()
                 {
                     Nome = "Casa"
@@ -28,6 +29,26 @@ namespace Aula1505_MVC1.Controllers
             ViewBag.MinhasCategorias = categorias;
 
             return View();
+        }
+
+
+
+        [HttpGet]
+        public ActionResult Categoria(string nome, string descricao, bool ativo)
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Categoria categoria)
+        {
+            //metodo para retornar os valores caso ocorra não ocorra o algum erro no cadastro de informações 
+            return View(categoria);
         }
     }
 }
