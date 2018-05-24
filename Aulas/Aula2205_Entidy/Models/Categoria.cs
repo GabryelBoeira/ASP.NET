16dd3450e03ea80e3b0674c34ba234d750271290
Gabryel <gabryel392@gmail.com>
2018-05-24 19:36:57 -0300

    Criação da model categoria

---
+++ Aulas/Aula2205_Entidy/Models/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Aula2205_Entidy.Models
+{
+    public class Categoria
+    {
+
+        public int CategoriaID { get; set; }
+
+        [Required, StringLength(30)]
+        public String Nome { get; set; }
+
+        public bool Ativo { get; set; }
+
+
+    }
+}
